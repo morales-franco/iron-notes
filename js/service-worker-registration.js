c6205a927a8ff8e1d6408ebf36b5a0953bc9d487
Morales Franco <franco.e.morales@gmail.com>
2019-08-04 22:00:12 +1200

    upload url

--- js/service-worker-registration.js
+++ js/service-worker-registration.js
@@ -1,5 +1,15 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-    .then(registration =>console.log('Registered:', registration))
-    .catch(error => console.log('Registration failed: ', error));
+// Registro Service Worker
+//URL PROD: https://morales-franco.github.io/iron-notes/index.html
+var url = window.location.href;
+var swLocation = "/iron-notes/service-worker.js"; //Production
+
+if (url.includes("localhost")) {
+  swLocation = "/service-worker.js";
+}
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register(swLocation)
+    .then(registration => console.log("Registered:", registration))
+    .catch(error => console.log("Registration failed: ", error));
 }
